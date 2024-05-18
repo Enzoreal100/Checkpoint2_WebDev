1f12c2f7caca5b4ab4beb0ea740ca4568be615c4
Enzoreal100 <enzoreal100@gmail.com>
2024-05-18 15:13:55 -0300

    Trabalho feito

--- Desktop/Checkpoint 2 - WebDev/scripts.js
+++ Desktop/Checkpoint 2 - WebDev/scripts.js
@@ -32,53 +32,104 @@ function calcula() {
     }
 
     document.getElementById('resCalculadora').innerHTML = `O resultado é: ${resultado}`;
+    setTimeout(() => {
+        do {
+            continuar = prompt("Deseja realizar outro cálculo? (S/N)").toLowerCase();
+            if (continuar !== "s" && continuar !== "n") {
+                alert("Resposta inválida. Digite 'S' ou 'N'.");
+            }
+        } while (continuar !== "s" && continuar !== "n");
+        if (continuar === "n") {
+            window.location.href = "https://www.youtube.com/watch?v=8G0_Gywm5kc";
+        }
+    }, 300);
 }
-// setTimeout(() => {
-//     do {
-//         continuar = prompt("Deseja realizar outro cálculo? (Sim/Não)").toLowerCase();
-//         if (continuar !== "sim" && continuar !== "não") {
-//             alert("Resposta inválida. Digite 'Sim' ou 'Não'.");
-//         }
-//     } while (continuar !== "sim" && continuar !== "não");
-//     if (continuar === "não") {
-//         window.location.href = "https://google.com/";
-//     }
-// }, 300);
+
+
 
 //ex2
 
 
 function IR(a) {
     if (a < 2800){
-        let aliquota = 0;
-        return a;
+        return 0;
     }
     else if (a < 5000){
-        let aliquota = 15;
-        return parseFloat(a*15/100);
+        return  a*15/100;
     }
     else
     {
-        let aliquota = 27;
-        return parseFloat(a*27/100);
+        return a*27/100;
     }
 }
 
 function FGTS(a){
-    return parseFloat(a*8/100);
+    return a*8/100;
 }
 
 function INSS(a){
-    return parseFloat(a*20/100);
+    return a*20/100;
 }
 
 function calculaSB(){
-    while (continua == 's'){
-        const bruto = window.prompt('Digite o valor de seu salário bruto').value;
-        resSB = document.getElementById('resSB');
-        resSB.innerHTML = `Salário líquido = ${bruto - (IR(bruto) + FGTS(bruto) + INSS(bruto))}`
-        setTimeout(300);
-        continua = window.prompt('Gostaria de fazer outra operação?');
+    const bruto = parseFloat(document.getElementById('sb').value);   
+    let sl = bruto - (IR(bruto) + FGTS(bruto)+ INSS(bruto));
+    document.getElementById('resSB').innerHTML = `Salário líquido = ${sl}`;
+
+    setTimeout(() => {
+        do {
+            continuar = prompt("Deseja realizar outro cálculo? (S/N)").toLowerCase();
+            if (continuar !== "s" && continuar !== "n") {
+                alert("Resposta inválida. Digite 'S' ou 'N'.");
+            }
+        } while (continuar !== "s" && continuar !== "n");
+        if (continuar === "n") {
+            window.location.href = "https://www.youtube.com/watch?v=s_eHTKuCkpc";
+        }
+    }, 300);
+}
+
+//Ex3
+
+function votacao(){
+    let votos1 = 0;
+    let votos2 = 0;
+    let votos3 = 0;
+    let votos4 = 0;
+    let votos5 = 0;
+    let nulos = 0;
+    while (true){;
+        let candidatos = 'Digite sua opção para prefeito\n 1 - João Melão \n 2 - Juninho Manella \n 3 - Batman \n 4 - João Gordo \n 5 - Lineuzinho\n 0 PARA ENCERRAR!';
+        let voto = window.prompt(`${candidatos}`)
+        switch(voto){
+            case '1':
+                window.alert('Voto Confirmado');
+                votos1++;
+                break;
+            case '2':
+                window.alert('Voto Confirmado');
+                votos2++
+                break;
+            case '3':
+                window.alert('Voto Confirmado');
+                votos3++
+                break;
+            case '4':
+                window.alert('Voto Confirmado');
+                votos4++
+                break;
+            case '5':
+                window.alert('Voto Confirmado');
+                votos5++;
+                break;
+            case '0':
+                window.alert(`VOTAÇÃO ENCERRADA!\n João Melão = ${votos1} votos.\n Juninho Manella = ${votos2} votos. \n Batman = ${votos3} votos. \n João Gordo = ${votos4} votos. \n Lineuzinho = ${votos5} votos. \n Nulos = ${nulos} votos.`);
+                return false;
+                break;
+            default:
+                window.alert('Voto Nulo');
+                nulos++;
+        }
     }
 }
 
